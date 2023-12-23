1da6e8036f1c816a8f9b01d2792220f115f5a2bb
System Administrator <root@MacBook-Pro-de-Garfias.local>
2023-12-22 22:11:06 -0600

    Seguimiento

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Perfil: {{ $user->username }}
+@endsection
+
+@section('contenido')
+    <div class=" w-full flex justify-center">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+            <div class="w-8/12 lg:w-6/12 px-5">
+                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen Usuario" />
+            </div>
+            <div class="md:w-8/12 lg:w-6/12 px-5  flex flex-col items-center md:justify-center py-10 md:items-start">
+                <p class="text-gray-700 text-2xl mt-5">{{ $user->username }}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">0
+                    <span class="font-normal">Siguiendo</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold"> 0
+                    <span class="font-normal">Seguiendo</span>
+                </p>
+
+                <p class="text-gray-800 text-sm mb-3 font-bold"> 0
+                    <span class="font-normal">Seguidores</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+        @if($posts->count())
+
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        @foreach ($posts as $post )
+            <div class="">
+                <a href="{{route('posts.show',['post'=>$post,'user'=>$user])}}">
+                    <img src="{{asset('/uploads')."/".$post->imagen}}" alt="Imagen del post {{ $post->titulo}}">
+                </a>
+            </div>              
+        @endforeach
+        </div>
+        <div>
+            {{$posts->links('pagination::tailwind')}}
+        </div>  
+        @else
+        <p class="text-gary-600 uppercase text-sm text-center font-bold">No hay posts</p>
+        @endif
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ComentarioController;
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +23,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/crea-cuenta', [RegisterController::class,'index'])->name('cuenta.crear');
+Route::post('/almacena-cuenta', [RegisterController::class,'store'])->name('cuenta.almacenar');
+
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
+Route::post('/login',[LoginController::class,'store']);
+
+Route::get('/{user:username}',[PostController::class,'index'])->name('posts.muro');
+Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
+Route::post('/posts/store',[PostController::class,'store'])->name('posts.store');
+Route::delete('posts/{post}',[PostController::class,'destroy'])->name('posts.delete');
+
+Route::get('/{user:username}/posts/{post}',[PostController::class,'show'])->name('posts.show');
+Route::post('/{user:username}/posts/{post}',[ComentarioController::class,'store'])->name('comentario.store');
+
+Route::post('/imagenes',[ImagenController::class,'store'])->name('imagenes.store');
+
+Route::post('/posts/{post}/likes',[LikeController::class,'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes',[LikeController::class,'destroy'])->name('posts.likes.destroy');
+
+
+
+
+
