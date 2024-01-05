80d8eafde80fce00df51855e1887b4748454276f
System Administrator <root@MacBook-Pro-de-Garfias.local>
2024-01-05 12:20:02 -0600

    Integracion de caracteriscas

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,19 @@ class User extends Authenticatable
     public function likes(){
         return $this->hasMany(Like::class);
     }
+
+    public function followers(){
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    //Comprobar si un usuario sigue a otro
+
+    public function siguiendo(User $user){
+        return $this->followers->contains($user->id);
+        
+    }
+
+    public function followings(){
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
 }
